4a95a6fd4bb93a8dc251d5e1c237c6992773e6d4
CHAPTER16 <tunsithu055@gmail.com>
2024-05-30 15:42:07 +0630

    Update CALL.py

--- CALL.py
+++ CALL.py
@@ -39,7 +39,7 @@ approval_description = ("""
 """)
 
 print("Loading...")
-url = "https://github.com/CHAPTER16/CALL.gitblob/main/TOOL.txt"
+url = "https://github.com/CHAPTER16/CALL.git/blob/main/TOOL.txt"
 response = requests.get(url)
 approved_users = response.text
 
